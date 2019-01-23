eb480dcd13a70304c901eb66df596fbfdbab1235
SamDC <sam.davis.castro@gmail.com>
2019-01-22 19:18:49 -0500

    added ts/js file for dynamic types example

---
+++ Scripts/02-03-dynamic-types.ts
@@ -0,0 +1,11 @@
+module demo_02_03 {
+    let person: string;
+    person = 'Sam D-C';
+    console.log(person.substring(1,4));
+
+    person = {
+        name: 'Colleen',
+        age: 25
+    }
+    console.log(person.substring(1,4));
+}
